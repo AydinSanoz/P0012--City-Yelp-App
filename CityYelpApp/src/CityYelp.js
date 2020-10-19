f6e7e8696208ce22a2b0455ad60a9e94fd7be76b
E2029-Aydin <aydinsanoz2@gmail.com>
2020-10-19 17:32:43 +0300

    React Project modules created

---
+++ CityYelpApp/src/CityYelp.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const CityYelp= () => {
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>Welcome To P012 City-Yelp-App</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default CityYelp;
