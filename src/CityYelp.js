d4c6178c0ab684060ca0d48c50fc8b8486b4b2fe
E2029-Aydin <aydinsanoz2@gmail.com>
2020-10-19 22:26:53 +0300

    second text added

--- src/CityYelp.js
+++ src/CityYelp.js
@@ -8,7 +8,8 @@ const CityYelp= () => {
     return(
         <SafeAreaView>
             <View>
-                <Text style = {{fontSize:20}}>Welcome To P012 City-Yelp-App</Text>
+                <Text style = {{fontSize:40}}>Welcome To P012 City-Yelp-App</Text>
+                <Text style = {{fontSize:40}}>Welcome To P012 City-Yelp-App</Text>
             </View>
         </SafeAreaView>
     )
